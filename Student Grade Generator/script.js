c07e3261e020516498eebd2819f54e4b500c9ef0
eliasndegwa <eliasndegwamwai@gmail.com>
2022-11-28 07:56:09 +0300

    added comments

--- Student Grade Generator/script.js
+++ Student Grade Generator/script.js
@@ -1,11 +1,13 @@
+//function for calcultating grades
 const calculate=()=>{
+    //getting grades from HTML
     let math=document.querySelector("#math").value
     let eng=document.querySelector("#eng").value
     let kisw=document.querySelector("#kisw").value
     let sci=document.querySelector("#sci").value
     let ss=document.querySelector("#ss").value
     let grades=""
-
+    //calculating total marks
     let totalGrades=
     parseFloat(math)+
     parseFloat(eng)+
@@ -13,7 +15,8 @@ const calculate=()=>{
     parseFloat(sci)+
     parseFloat(ss)
 
-    let percentage = (totalgrades / 500) * 100;
+    //assigning grades
+    let percentage = (totalGrades / 500) * 100;
     if (percentage>79) {
         grades="A"
     } else if (percentage>59&&percentage<=79) {
@@ -26,15 +29,17 @@ const calculate=()=>{
         grades="E"
     }
 
+    //ensuring values are not empty
     if (math==""|| eng==""|| kisw==""|| sci==""|| ss=="") {
         document.querySelector("#showdata").innerHTML="Please enter all the fields"
     } else {
+        //returning the result
         if (percentage>=40) {
-            document.querySelector("#showdata").innerHTML=`Out of 400 your total is  ${totalgrades} 
+            document.querySelector("#showdata").innerHTML=`Out of 500 your total is  ${totalGrades} 
         and percentage is ${percentage}%. <br> 
         Your grade is ${grades}. You have Passed.`
         } else {
-            document.querySelector("#showdata").innerHTML=`Out of 400 your total is  ${totalgrades} 
+            document.querySelector("#showdata").innerHTML=`Out of 500 your total is  ${totalGrades} 
         and percentage is ${percentage}%. <br> 
         Your grade is ${grades}. You have failed.`
         }
